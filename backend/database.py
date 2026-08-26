8018cfeb2291202a216857451728e0c39fc0a161
Thilitshi <thilitshimudzungwane@gmail.com>
2026-08-26 12:32:43 +0200

    Configure Aiven MySQL SSL connection

--- backend/database.py
+++ backend/database.py
@@ -11,15 +11,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is missing")
 
-if DATABASE_URL.startswith("mysql://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "mysql://",
-        "mysql+pymysql://",
-        1
-    )
+DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
+DATABASE_URL = DATABASE_URL.split("?")[0]
 
 engine = create_engine(
     DATABASE_URL,
+    connect_args={
+        "ssl": {}
+    },
     echo=False
 )
 
